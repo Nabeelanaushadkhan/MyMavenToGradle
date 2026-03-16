034800aafe1ed8329fa17bdd52264ea45e1c561e
Nabeelanaushadkhan <nabeelakhan9869@gmail.com>
2026-03-16 19:15:31 +0000

    first commit

--- MyMavenSeleniumApp01/src/main/java/com/example/App.java
+++ MyMavenSeleniumApp01/src/main/java/com/example/App.java
@@ -1,25 +1,35 @@
 package com.example;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WindowType;
 
 public class App {
+
     public static void main(String[] args) throws InterruptedException {
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://practicetestautomation.com/practice-test-login/");
+        System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+
+        WebDriver driver = new FirefoxDriver();
+
         driver.manage().window().maximize();
-        
+
+        // Open first website
+        driver.get("https://www.saucedemo.com/");
         Thread.sleep(5000);
 
-        driver.findElement(By.id("username")).sendKeys("student");
+        // Open second website
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://automationexercise.com/");
         Thread.sleep(5000);
-        driver.findElement(By.id("password")).sendKeys("Password123");
-        Thread.sleep(5000);
-        driver.findElement(By.id("submit")).click();
 
+        // Open third website
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://practicetestautomation.com/");
         Thread.sleep(5000);
+
+        // Keep browser open for 10 seconds
+        Thread.sleep(10000);
 
         driver.quit();
     }
